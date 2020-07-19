b5a8072c038813764db9c5185777474072834b5e
rishabhmthakur2 <rishabhmthakur@outlook.com>
2020-07-19 14:55:53 +0530

    Dapp for Test Net

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const path = require("path");
+const hbs = require("hbs");
+const BN = require('bn.js');
+
+const app = express();
+const port = process.env.PORT || 3000;
+const publicDirectoryPath = path.join(__dirname, "./public");
+const viewsPath = path.join(__dirname, "./templates/views");
+const partialsPath = path.join(__dirname, "./templates/partials");
+
+app.set("view engine", "hbs");
+app.set("views", viewsPath);
+hbs.registerPartials(partialsPath);
+
+app.use(express.static(publicDirectoryPath));
+
+app.get("", (req, res) => {
+    res.render("index");
+});
+
+app.listen(port, () => {
+    console.log("Server is listening for calls.");
+});
